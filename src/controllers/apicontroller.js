3370065d2de9d525c875eb9ce62156d5e58884e4
Shivam Bharti <bhartishivam723@gmail.com>
2022-05-17 20:52:09 +0530

    adding readme file

--- src/controllers/apicontroller.js
+++ src/controllers/apicontroller.js
@@ -1,10 +1,3 @@
-var movies = [
-    { id: 101, name: "Fight Club", year: 1999, rating: 8.1 },
-    { id: 102, name: "Inception", year: 2010, rating: 8.7 },
-    { id: 103, name: "The Dark Knight", year: 2008, rating: 9 },
-    { id: 104, name: "12 Angry Men", year: 1957, rating: 8.9 }
-];
-
 const employeeModel = require('../models/employee')
 
 const index = async (req, res) => {
